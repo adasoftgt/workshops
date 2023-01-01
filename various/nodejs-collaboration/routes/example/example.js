a72780ff2c78ad6dc2e0f98a4b12fc50ac236e7a
W Guay Sen Rafael Herrador Reyes <2214940300101@ingenieria.usac.edu.gt>
2023-01-01 13:26:34 -0600

    Se agrego cambios nodejs colaborativo

--- various/nodejs-collaboration/routes/example/example.js
+++ various/nodejs-collaboration/routes/example/example.js
@@ -3,7 +3,7 @@ var upload = multer({ dest: 'uploads/' })
 
 module.exports = (express, app) => {
 
-    app.post('/helloword', upload.any(), async function (req, res) {
+    app.get('/helloword', upload.any(), async function (req, res) {
       return res.status(200).send('hello word')
     });
 }
